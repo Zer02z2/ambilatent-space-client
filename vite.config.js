22b112762f132fa6e2b1699a2afae74c4114f1ac
Zer02z2 <zongzechan@gmail.com>
2025-05-06 05:41:31 -0400

    Build with correct base

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   },
   root: resolve(__dirname, "src"),
   publicDir: resolve(__dirname, "public"),
-  base: "/ambilatent/",
+  base: "/ambilatent-space-client/",
   build: {
     outDir: "../docs",
   },
